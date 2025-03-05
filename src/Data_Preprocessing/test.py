8438cd2d3a6e9e3b1ced4cdc8661b2033a0a1013
Dug <lucadughera@gmail.com>
2025-03-05 14:17:43 +0100

    changed the test file

--- src/Data_Preprocessing/test.py
+++ src/Data_Preprocessing/test.py
@@ -1,1 +1,11 @@
 print("finally it works")
+
+# Define the filename
+filename = "here I am!.txt"
+
+# Open the file in write mode, creating it if it doesn't exist
+with open(filename, 'w') as file:
+    # Write some text to the file
+    file.write("Hello, this is a simple text file!")
+
+print(f"File '{filename}' has been created and written to.")
