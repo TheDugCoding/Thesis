06cacb819e7293369b7d8ede61b5fc3aae09c572
Dug <lucadughera@gmail.com>
2025-04-28 16:26:51 +0200

    removed dropout from gat

--- src/modeling/downstream_task/GAT_elliptic.py
+++ src/modeling/downstream_task/GAT_elliptic.py
@@ -29,16 +29,16 @@ else:
 class GAT(torch.nn.Module):
     def __init__(self, in_channels, hidden_channels, out_channels, heads):
         super().__init__()
-        self.conv1 = GATConv(in_channels, hidden_channels, heads, dropout=0.6)
+        self.conv1 = GATConv(in_channels, hidden_channels, heads)
         # On the Pubmed dataset, use `heads` output heads in `conv2`.
         self.conv2 = GATConv(hidden_channels * heads, out_channels, heads=1,
-                             concat=False, dropout=0.6)
+                             concat=False)
 
     def forward(self, x, edge_index):
         #Dropout helps prevent overfitting by randomly nullifying outputs from neurons during the training process. This encourages the network to learn redundant representations for everything and hence, increases the model's ability to generalize.
-        x = F.dropout(x, p=0.6, training=self.training)
+        #x = F.dropout(x, p=0.6, training=self.training)
         x = F.elu(self.conv1(x, edge_index))
-        x = F.dropout(x, p=0.6, training=self.training)
+        #x = F.dropout(x, p=0.6, training=self.training)
         x = self.conv2(x, edge_index)
         return x
 
