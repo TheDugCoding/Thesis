5220cca93c0fdd99974b9344ac678808be8222ee
Dug <lucadughera@gmail.com>
2025-06-16 11:49:45 +0200

    fixes

--- src/data_preprocessing/preprocess.py
+++ src/data_preprocessing/preprocess.py
@@ -566,7 +566,7 @@ class EllipticDataset(Dataset):
 
     def get(self, idx):
         """Loads and returns the graph at the given index."""
-        data = torch.load(os.path.join(self.processed_dir, f'ellipticdataset_{idx}.pt'), weights_only=False)
+        data = torch.load(os.path.join(self.processed_dir, f'ellipticdataset_{idx}.pt'))
         return data
 
 
